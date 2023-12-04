f14bcc2640831c2d64e584df28e43889bdf34f24
Christopher Said <christopher.w.said@gmail.com>
2023-12-04 08:55:21 -0600

    Initial commit

---
+++ src/view/StartGenerator.java
@@ -0,0 +1,30 @@
+package view;
+
+/**  
+* Christopher Said - cwsaid  
+* CIS171 <11961>
+* Dec 4, 2023  
+*/
+
+public class StartGenerator {
+
+	public static void main(String[] args) {
+		
+		/*	Algorithm
+		 * 		start program
+		 * 		render GUI fields and buttons
+		 * 		get book title, pages, days from user
+		 * 		calculate pages per day
+		 * 		add title and pages to array 
+		 * 		get current date and from that assign dates to each days pages
+		 * 		display date, title, pages per day in pane
+		 * 		repeat user input, calculations, and display until user prints output
+		 * 		when user finished output contents of pane to txt file
+		 * 		end program
+		*/
+		
+		System.out.println("Hello!");
+		
+	}
+
+}
